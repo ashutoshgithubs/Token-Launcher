0010f8a22f6704e35f67f985ceb50cdcd137a541
Ashutosh <kumarashutosh1956@gmail.com>
2024-10-19 22:37:39 +0530

    Cards made responsive

--- src/components/ToolCards.jsx
+++ src/components/ToolCards.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
 import { useNavigate } from 'react-router-dom';
@@ -11,7 +11,22 @@ import { Autoplay, FreeMode, Pagination } from "swiper/modules";
 
 export default function ToolCards() {
     const navigate = useNavigate();
-    const { connected } = useWallet(); 
+    const { connected } = useWallet();
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 768); 
+        };
+
+        handleResize(); 
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     function handleAirdrop() {
         if (!connected) {
@@ -48,15 +63,43 @@ export default function ToolCards() {
             navigate("/view-tokens");
         }
     }
+
     function handleLiquidity() {
         if (!connected) {
             alert("Please connect your wallet");
             navigate("/");
         } else {
-            alert("This feature is Under development. It will be available soon!")
-            // navigate("/view-tokens");
+            alert("This feature is under development. It will be available soon!");
         }
     }
+
+    const cards = [
+        {
+            title: "Airdrop Solana",
+            description: "Request free SOL for testing & development",
+            action: handleAirdrop,
+        },
+        {
+            title: "Generate Token",
+            description: "Create Solana Token with Personalized Metadata",
+            action: handleGenerateToken,
+        },
+        {
+            title: "Send Solana",
+            description: "Send SOL to Any Wallet with ease",
+            action: handleSendSolana,
+        },
+        {
+            title: "View Tokens",
+            description: "Tokens associated with the linked account.",
+            action: handleViewTokens,
+        },
+        {
+            title: "Liquidity Pools",
+            description: "Contribute assets to facilitate trading.",
+            action: handleLiquidity,
+        }
+    ];
 
     return (
         <div id="tools" className="max-w-6xl mx-auto">
@@ -65,99 +108,52 @@ export default function ToolCards() {
                 <p className="text-xl">Create, launch, and manage your own Solana tokens</p>
             </div>
 
-    
-            <Swiper
-    slidesPerView={3}
-    spaceBetween={30}
-    loop={true}
-    freeMode={true}
-    autoplay={{
-        delay: 3000,
-        disableOnInteraction: false,
-    }}
-    pagination={{
-        clickable: true,
-    }}
-    modules={[FreeMode, Pagination, Autoplay]}
-    className="mySwiper"
-    breakpoints={{
-        // When the screen width is 640px or smaller, show 1 slide per view
-        0: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        // When the screen width is 768px or larger, show 2 slides per view
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        // When the screen width is 1024px or larger, show 3 slides per view
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-    }}
->
-    <SwiperSlide>
-        <Card className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
-            <CardHeader>
-                <CardTitle>Airdrop Solana</CardTitle>
-                <CardDescription>Request free SOL for testing & development</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Button onClick={handleAirdrop}>Get Started</Button>
-            </CardContent>
-        </Card>
-    </SwiperSlide>
-
-    <SwiperSlide>
-        <Card className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
-            <CardHeader>
-                <CardTitle>Generate Token</CardTitle>
-                <CardDescription>Create Solana Token with Personalized Metadata</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Button onClick={handleGenerateToken}>Get Started</Button>
-            </CardContent>
-        </Card>
-    </SwiperSlide>
-
-    <SwiperSlide>
-        <Card className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
-            <CardHeader>
-                <CardTitle>Send Solana</CardTitle>
-                <CardDescription>Send SOL to Any Wallet with ease</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Button onClick={handleSendSolana}>Get Started</Button>
-            </CardContent>
-        </Card>
-    </SwiperSlide>
-
-    <SwiperSlide>
-        <Card className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
-            <CardHeader>
-                <CardTitle>View Tokens</CardTitle>
-                <CardDescription>Tokens associated with the linked account.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Button onClick={handleViewTokens} >Get Started</Button>
-            </CardContent>
-        </Card>
-    </SwiperSlide>
-    <SwiperSlide>
-        <Card className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
-            <CardHeader>
-                <CardTitle>Liquidity Pools</CardTitle>
-                <CardDescription>Contribute assets to facilitate trading.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Button onClick={handleLiquidity} >Get Started</Button>
-            </CardContent>
-        </Card>
-    </SwiperSlide>
-</Swiper>
-
+            {isSmallScreen ? (
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={20}
+                    loop={true}
+                    freeMode={true}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[FreeMode, Pagination, Autoplay]}
+                    className="mySwiper"
+                >
+                    {cards.map((card, index) => (
+                        <SwiperSlide key={index}>
+                            <Card className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
+                                <CardHeader>
+                                    <CardTitle>{card.title}</CardTitle>
+                                    <CardDescription>{card.description}</CardDescription>
+                                </CardHeader>
+                                <CardContent>
+                                    <Button onClick={card.action}>Get Started</Button>
+                                </CardContent>
+                            </Card>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            ) : (
+        
+                <div className="grid grid-cols-3 gap-6">
+                    {cards.map((card, index) => (
+                        <Card key={index} className="shadow-sm hover:shadow-md transition-shadow duration-300 shadow-slate-400 hover:scale-105">
+                            <CardHeader>
+                                <CardTitle>{card.title}</CardTitle>
+                                <CardDescription>{card.description}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <Button onClick={card.action}>Get Started</Button>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
